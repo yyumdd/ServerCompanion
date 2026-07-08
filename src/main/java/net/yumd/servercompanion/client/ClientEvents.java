42d6921fe5d189bb78ede9eb06ee18b3756e382a
yyumdd <daniel.a.shepherd2012@gmail.com>
2026-07-08 16:30:59 +0200

    Add client mod list reporting packet

--- src/main/java/net/yumd/servercompanion/client/ClientEvents.java
+++ src/main/java/net/yumd/servercompanion/client/ClientEvents.java
@@ -7,6 +7,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.yumd.servercompanion.ServerCompanion;
 import net.yumd.servercompanion.network.ModMessages;
 import net.yumd.servercompanion.network.packet.HelloC2SPacket;
+import net.minecraftforge.fml.ModList;
+import java.util.ArrayList;
+import java.util.List;
 
 @Mod.EventBusSubscriber(
         modid = ServerCompanion.MOD_ID,
@@ -14,13 +17,27 @@ import net.yumd.servercompanion.network.packet.HelloC2SPacket;
 )
 public class ClientEvents {
 
+    private static List<String> getInstalledMods() {
+        var mods = ModList.get().getMods();
+        var modList = new ArrayList<String>();
+
+        for (var mod : mods) {
+            modList.add(mod.getModId());
+        }
+
+        return modList;
+    }
+
     @SubscribeEvent
     public static void onLoggingIn(ClientPlayerNetworkEvent.LoggingIn event) {
 
         System.out.println("ServerCompanion sending hello packet!");
+        var mods = getInstalledMods();
+
+        System.out.println(mods);
 
         ModMessages.INSTANCE.sendToServer(
-                new HelloC2SPacket(ServerCompanion.VERSION)
+                new HelloC2SPacket(ServerCompanion.VERSION, mods)
         );
     }
 }
--- src/main/java/net/yumd/servercompanion/network/packet/HelloC2SPacket.java
+++ src/main/java/net/yumd/servercompanion/network/packet/HelloC2SPacket.java
@@ -7,20 +7,37 @@ import net.yumd.servercompanion.ServerCompanion;
 import net.yumd.servercompanion.server.VerificationManager;
 
 import java.util.function.Supplier;
+import java.util.List;
+import java.util.ArrayList;
 
 public class HelloC2SPacket {
     private final String version;
+    private final List<String> mods;
 
-    public HelloC2SPacket(String version) {
+    public HelloC2SPacket(String version, List<String> mods) {
         this.version = version;
+        this.mods = mods;
     }
 
     public HelloC2SPacket(FriendlyByteBuf buffer) {
+
         version = buffer.readUtf();
+
+        int modCount = buffer.readInt();
+
+        mods = new ArrayList<>();
+
+        for (int i = 0; i < modCount; i++) {
+            mods.add(buffer.readUtf());
+        }
     }
 
     public void encode(FriendlyByteBuf buffer) {
         buffer.writeUtf(version);
+        buffer.writeInt(mods.size());
+        for (var mod : mods) {
+            buffer.writeUtf(mod);
+        }
     }
 
     public boolean handle(Supplier<NetworkEvent.Context> supplier) {
@@ -39,6 +56,10 @@ public class HelloC2SPacket {
                 ServerCompanion.LOGGER.info(
                         "ServerCompanion version: {}",
                         version
+                );
+                ServerCompanion.LOGGER.info(
+                        "Client mods: {}",
+                        mods
                 );
                 VerificationManager.verify(sender.getUUID());
             } else {
