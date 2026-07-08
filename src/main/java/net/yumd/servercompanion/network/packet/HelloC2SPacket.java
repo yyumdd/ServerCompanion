318df0388f9deda642e4113cde96a32fea57bf7f
yyumdd <daniel.a.shepherd2012@gmail.com>
2026-07-08 14:52:14 +0200

    Add version data to client handshake packet

--- src/main/java/net/yumd/servercompanion/network/packet/HelloC2SPacket.java
+++ src/main/java/net/yumd/servercompanion/network/packet/HelloC2SPacket.java
@@ -9,17 +9,18 @@ import net.yumd.servercompanion.server.VerificationManager;
 import java.util.function.Supplier;
 
 public class HelloC2SPacket {
+    private final String version;
 
-    public HelloC2SPacket() {
-
+    public HelloC2SPacket(String version) {
+        this.version = version;
     }
 
     public HelloC2SPacket(FriendlyByteBuf buffer) {
-
+        version = buffer.readUtf();
     }
 
     public void encode(FriendlyByteBuf buffer) {
-
+        buffer.writeUtf(version);
     }
 
     public boolean handle(Supplier<NetworkEvent.Context> supplier) {
@@ -35,7 +36,10 @@ public class HelloC2SPacket {
                         "Received hello from {}",
                         sender.getName().getString()
                 );
-
+                ServerCompanion.LOGGER.info(
+                        "ServerCompanion version: {}",
+                        version
+                );
                 VerificationManager.verify(sender.getUUID());
             } else {
                 System.out.println("ServerCompanion received packet but sender was null");
